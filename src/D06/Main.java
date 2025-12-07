65b2c83a4b0d15ae613d731e8d3257b0852f4bce
krubik66 <krubik66@gmail.com>
2025-12-07 20:01:26 +0100

    Day 6 started.

---
+++ src/D06/Main.java
@@ -0,0 +1,27 @@
+package D06;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "src/D05/fullFile.txt";
+        String filePath2 = "src/D05/testFile.txt";
+        MathIsEasy firstCharge = new MathIsEasy(true);
+        MathIsEasy secondCharge = new MathIsEasy(false);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                firstCharge.loadTheCharge(line);
+                secondCharge.loadTheCharge(line);
+            }
+            System.out.println("First charge:\t" + firstCharge.getSumOfFresh());
+            System.out.println("Second charge:\t" + secondCharge.getSumOfFresh());
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + filePath2);
+            e.printStackTrace();
+        }
+    }
+}
